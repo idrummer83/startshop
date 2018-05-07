eb5f08a430275e418c88c94b863aa2846006e625
idrummer83 <vodaadminwater@gmail.com>
2018-05-07 23:51:59 +0300

    models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from tabbed_admin import TabbedModelAdmin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(TabbedModelAdmin):
+    model = Product
+    list_display = ('id', 'name', 'slug', 'published', 'image', 'description', 'long_description')
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from filer.fields.image import FilerImageField
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+class baseImage(models.Model):
+     image = FilerImageField(verbose_name='Изображение', on_delete=models.CASCADE, null=True, blank=True,
+                          related_name='photo')
+
+
+class productCategory(models.Model):
+    name = models.CharField('наименование категории', max_length=250)
+    slug = models.SlugField(verbose_name='Slug', max_length=64, unique=True)
+    published = models.BooleanField(verbose_name='Опубликовано', default=True)
+    image = models.ForeignKey(baseImage, on_delete=models.CASCADE, related_name='product_image', upload_to='product_photo', verbose_name='изображение категории')
+    description = RichTextField(verbose_name='Описание', null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'категория продукта'
+        verbose_name = 'категория продукта'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(productCategory):
+    long_description = RichTextField(verbose_name='Описание', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('name',)
