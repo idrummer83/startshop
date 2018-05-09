68cbfcaa1394053c835c4f3276c43cd30020b5b1
idrummer83 <vodaadminwater@gmail.com>
2018-05-10 00:16:24 +0300

    models

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from tabbed_admin import TabbedModelAdmin
+# from sorl.thumbnail.admin import AdminImageMixin
+
 from .models import *
 
 # Register your models here.
 
-# @admin.register(Product)
-class ProductAdmin(models.Model):
-    model = productCategory
+@admin.register(baseImage)
+class baseImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(productCategory)
+class productCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'published', 'image', 'description')
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug', 'published', 'image', 'description', 'long_description')
--- shop/models.py
+++ shop/models.py
@@ -8,12 +8,16 @@ class baseImage(models.Model):
      image = FilerImageField(verbose_name='Изображение', on_delete=models.CASCADE, null=True, blank=True,
                           related_name='photo')
 
+     class Meta:
+         verbose_name_plural = 'Изображение'
+         verbose_name = 'Изображение'
+
 
 class productCategory(models.Model):
     name = models.CharField('наименование категории', max_length=250)
     slug = models.SlugField(verbose_name='Slug', max_length=64, unique=True)
     published = models.BooleanField(verbose_name='Опубликовано', default=True)
-    image = models.ForeignKey(baseImage, on_delete=models.CASCADE, related_name='product_image', verbose_name='изображение категории')
+    image = models.ForeignKey(baseImage, on_delete=models.CASCADE, related_name='product_cat_image', verbose_name='изображение категории')
     # image = FilerImageField(verbose_name='Изображение категории', related_name='product_image', null=True, blank=True)
     description = RichTextField(verbose_name='Описание', null=True, blank=True)
 
@@ -26,10 +30,19 @@ class productCategory(models.Model):
 
 
 
-class Product(productCategory):
-    long_description = RichTextField(verbose_name='Описание', null=True, blank=True)
+class Product(models.Model):
+    category = models.ForeignKey(productCategory, on_delete=models.CASCADE, related_name='prod_of_category', verbose_name='категория', blank=True)
+    name = models.CharField('наименование продукта', max_length=250)
+    slug = models.SlugField(verbose_name='Slug', max_length=64, unique=True)
+    published = models.BooleanField(verbose_name='Опубликовано', default=True)
+    image = models.ForeignKey(baseImage, on_delete=models.CASCADE, related_name='product_image',
+                              verbose_name='изображение продукта')
+    description = RichTextField(verbose_name='Описание', null=True, blank=True)
+    long_description = RichTextField(verbose_name='Описание большое', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
