253a6a2c9a216b5ef6c5f830b54ced4fd22ff889
idrummer83 <vodaadminwater@gmail.com>
2018-05-11 00:08:02 +0300

    html, static

--- startshop/settings.py
+++ startshop/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print(BASE_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -134,21 +136,8 @@ CKEDITOR_CONFIGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "startshop/static"),
-        'static',
-    ]
-    MEDIA_DIRS = [
-        os.path.join(BASE_DIR, "startshop/media"),
-        'media',
-    ]
-    MEDIA_ROOT = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)),
-        'media',
-    )
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'startshop/media')
