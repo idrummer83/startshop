ea3cda77e1a26072e58b8d87b775824137070103
idrummer83 <vodaadminwater@gmail.com>
2018-09-03 23:48:47 +0300

    static

--- startshop/settings.py
+++ startshop/settings.py
@@ -162,7 +162,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'startshop/static/'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'startshop/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
